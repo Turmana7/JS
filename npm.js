2457287b0d0c188c66567930fb03d42ace42d016
saba <saba.turmanidze2014@gmail.com>
2025-08-03 02:11:42 +0400

    npm

---
+++ npm.js
@@ -0,0 +1,6 @@
+import isOdd from "is-odd";//npm i "packages saxeli" ase shegvidzlia gadmoviwerot sxva developerebis nawerri kodi da gamoviyenot sakutar kodshi,aseve shegvidzlia washla uninstallis sashualebit 
+
+console.log(isOdd(123));
+//failis mjs-ad gadaketebis magivrad chven shegvidzlia amave failis json failshi mivutitot "type" :"module";
+
+//npm outdate-ti shegvidzlia vnaxot gasanaxlebeli packagebi da npm update brdzanebit gavanaxlot(did proeqtebshi frttxilad unda viyot updates dros :DD)
